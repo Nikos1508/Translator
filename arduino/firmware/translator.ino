9e289019c2b58376676fab6bf3174cb3f86f0fad
Adrian Rubio-Punal <adrian.rubio.punal@gmail.com>
2025-08-09 13:24:34 +0200

    Update tunes and running logic

--- arduino/firmware/translator.ino
+++ arduino/firmware/translator.ino
@@ -1,8 +1,10 @@
-// c++
 const byte piezoPin = 6;
 
-// piezo frequency
-const unsigned int feq[5] = {0, 261, 349, 440, 523};
+// store previous button states
+bool lastGreen = HIGH;
+bool lastYellow = HIGH;
+bool lastRed = HIGH;
+bool lastBlue = HIGH;
 
 void setup() {
   // leds
@@ -21,38 +23,103 @@ void setup() {
   pinMode(piezoPin, OUTPUT);
 
   Serial.begin(9600);
+
+  startupSound();  // Play startup sound once at boot
 }
 
 void loop() {
-  if (digitalRead(8) == LOW) {
+  bool green = digitalRead(8);
+  bool yellow = digitalRead(9);
+  bool red = digitalRead(10);
+  bool blue = digitalRead(11);
+
+  if (green == LOW) {
     digitalWrite(2, HIGH);
-    tone(piezoPin, feq[1]);
-    delay(100);
-    noTone(piezoPin);
+    if (lastGreen == HIGH) {
+      greenTune();
+      delay(50);  // small debounce delay
+    }
+  } else {
     digitalWrite(2, LOW);
   }
 
-  if (digitalRead(9) == LOW) {
+  if (yellow == LOW) {
     digitalWrite(3, HIGH);
-    tone(piezoPin, feq[2]);
-    delay(100);
-    noTone(piezoPin);
+    if (lastYellow == HIGH) {
+      yellowTune();
+      delay(50);
+    }
+  } else {
     digitalWrite(3, LOW);
   }
 
-  if (digitalRead(10) == LOW) {
+  if (red == LOW) {
     digitalWrite(4, HIGH);
-    tone(piezoPin, feq[3]);
-    delay(100);
-    noTone(piezoPin);
+    if (lastRed == HIGH) {
+      redTune();
+      delay(50);
+    }
+  } else {
     digitalWrite(4, LOW);
   }
 
-  if (digitalRead(11) == LOW) {
+  if (blue == LOW) {
     digitalWrite(5, HIGH);
-    tone(piezoPin, feq[4]);
-    delay(100);
-    noTone(piezoPin);
+    if (lastBlue == HIGH) {
+      blueTune();
+      delay(50);
+    }
+  } else {
     digitalWrite(5, LOW);
   }
+
+  lastGreen = green;
+  lastYellow = yellow;
+  lastRed = red;
+  lastBlue = blue;
+}
+
+// two-tone tunes for each color
+
+void greenTune() {
+  tone(piezoPin, 523, 150);
+  delay(150);
+  tone(piezoPin, 659, 150);
+  delay(150);
+  noTone(piezoPin);
+}
+
+void yellowTune() {
+  tone(piezoPin, 392, 150);
+  delay(150);
+  tone(piezoPin, 440, 150);
+  delay(150);
+  noTone(piezoPin);
+}
+
+void redTune() {
+  tone(piezoPin, 330, 150);
+  delay(150);
+  tone(piezoPin, 294, 150);
+  delay(150);
+  noTone(piezoPin);
+}
+
+void blueTune() {
+  tone(piezoPin, 262, 150);
+  delay(150);
+  tone(piezoPin, 311, 150);
+  delay(150);
+  noTone(piezoPin);
+}
+
+// Startup sound
+void startupSound() {
+  tone(piezoPin, 400, 150);
+  delay(200);
+  tone(piezoPin, 600, 150);
+  delay(200);
+  tone(piezoPin, 800, 300);
+  delay(300);
+  noTone(piezoPin);
 }
